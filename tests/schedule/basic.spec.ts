bc37057fbd1d1a56442a216320ae5c34adbbbc15
Leehyunji <hjlee@bizflow.com>
2025-11-02 22:07:28 +0900

    setup: 빈 테스트 파일 생성

---
+++ tests/schedule/basic.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
+
+test('일정 Create', async ({ page }) => {
+  await page.getByLabel('제목').fill('일정 추가 테스트');
+  //   await page.getByLabel('날짜').fill('일정 추가 테스트');
+});
+
+test('일정 Read', () => {}); // 주간 뷰, 월간 뷰.
+
+test('일정 Update', () => {});
+
+test('일정 Delete', () => {});
