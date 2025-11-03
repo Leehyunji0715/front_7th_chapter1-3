de371b61e542fb8267e02ddb67da74b8e9d6c57d
Leehyunji0715 <hjlee@bizflow.com>
2025-11-03 14:48:21 +0900

    임시 comment

--- tests/schedule/basic.spec.ts
+++ tests/schedule/basic.spec.ts
@@ -1,16 +1,15 @@
-import { test } from '@playwright/test';
+// import { test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
+// test.beforeEach(async ({ page }) => {
+//   await page.goto('/');
+// });
 
-test('일정 Create', async ({ page }) => {
-  await page.getByLabel('제목').fill('일정 추가 테스트');
-  //   await page.getByLabel('날짜').fill('일정 추가 테스트');
-});
+// test('일정 Create', async ({ page }) => {
+//   await page.getByLabel('제목').fill('일정 추가 테스트');
+// });
 
-test('일정 Read', () => {}); // 주간 뷰, 월간 뷰.
+// test('일정 Read', () => {}); // 주간 뷰, 월간 뷰.
 
-test('일정 Update', () => {});
+// test('일정 Update', () => {});
 
-test('일정 Delete', () => {});
+// test('일정 Delete', () => {});
